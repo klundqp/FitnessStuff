a9f65871f1f8b0c3fb0cd7e03f202ede730a8e69
Kasper Lund <klundqp@gmail.com>
2023-07-16 10:37:21 +0200

    Add navigation bar

---
+++ fitnessstuff/src/components/Bootstrap/Navigation/subcomponents/NavbarBrand.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <a class="navbar-brand" href="#">
+        <slot></slot>
+    </a>
+</template>
---
+++ fitnessstuff/src/components/Bootstrap/Navigation/subcomponents/NavbarNav.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ id: string }>();
+</script>
+<template>
+    <div class="collapse navbar-collapse" :id="id">
+        <div class="navbar-nav">
+            <slot></slot>
+        </div>
+    </div>
+</template>
