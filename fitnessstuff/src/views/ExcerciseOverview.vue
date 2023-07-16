d0163539f4a6bbd46f6fb666f7d332ca629afe7b
Kasper Lund <klundqp@gmail.com>
2023-07-16 14:12:13 +0200

    Implement baseTable and add excercise table

--- fitnessstuff/src/views/ExcerciseOverview.vue
+++ fitnessstuff/src/views/ExcerciseOverview.vue
@@ -1,15 +1,72 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import Container from "@/components/Bootstrap/layout/Container.vue";
+import Col from "@/components/Bootstrap/layout/Col.vue";
+import Row from "@/components/Bootstrap/layout/Row.vue";
+import type { TableField } from "@/components/Bootstrap/BaseTable.vue";
+import ExerciseTable from "@/components/ExerciseTable.vue";
 import { useExcerciseOverviewStore } from '../stores/excerciseOverviewStore';
 
 const store = useExcerciseOverviewStore();
+
+onMounted(() => {
+  store.load();
+});
+
+const fields: TableField[] = [
+  {
+    key: "name",
+    label: "Navn"
+  },
+  {
+    key: "category",
+    label: "Kategori"
+  },
+  {
+    key: "force",
+    label: "Type",
+    thClass: "text-right",
+    tdClass: "text-right"
+  },
+  {
+    key: "level",
+    label: "Sværhedsgrad",
+    thClass: "text-right",
+    tdClass: "text-right"
+  },
+  {
+    key: "primaryMuscles",
+    label: "Primære muskelgrupper",
+    thClass: "text-right",
+    tdClass: "text-right",
+    sortable: false
+  },
+  {
+    key: "secondaryMuscles",
+    label: "Sekundære muskelgrupper",
+    thClass: "text-right",
+    tdClass: "text-right",
+    sortable: false
+  }
+];
+
 </script>
 
 <template>
-  <div class="overview">
-    <h1>ExcerciseOverview</h1>
-    <div class="input-div">
-      <input type="text" v-model="store.text" placeholder="Excercise">
-      <router-link :to="'/exercise/' + store.text">To excercise</router-link>
-    </div>
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <h1>ExcerciseOverview</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <input type="text" v-model="store.text" placeholder="Excercise">
+      </Col>
+    </Row>
+    <Row>
+      <ExerciseTable :items="store.excerciseList" :fields="fields" sortable>
+      </ExerciseTable>
+    </Row>
+  </Container>
 </template>
